ae0ee4042397c05db0ced03e4d5750f505efe59b
Juanfran <juanfranj@hotmail.com>
2023-10-30 22:10:05 +0100

    popup

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class Mensaje_Popup(Popup):
         #self.cargar_banner()
     
     def mostrar_banner(self):
-        
+        self.app.banner_ppal.hide()
         self.banner_popup = BannerAd("ca-app-pub-3378097856628013/9783845500", "BANNER", False)
         self.banner_popup.show()
 
